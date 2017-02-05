d999b4b3af91e97e3f72d09224d82dd4b153c232
Alexander Echevarria <alexanderechevarria@DHCP-61-190.ST.HMC.Edu>
2017-02-04 16:56:04 -0800

    mostly works. unexpected \r bug

--- TableCreator.py
+++ TableCreator.py
@@ -31,91 +31,82 @@ class TableCreator:
         self.files = ""
 
     # Public Methods 
-    def go(name):
-        """
-        User provides the name of the tables to be created
-        """
-        sampleDay = os.listdir(desktopLoc)
-        lDList = map(lambda x: processExp(getData(x)), sampleDay)
-        zz = reduce(combineData, lDList)
-        commaSeparatedElements = LOLToCSV(zz)
-        writeString = titleString(dateString) + restString(commaSeparatedElements) 
-        f = open(dateString + "Data.txt", 'w')
-        f.write(writeString)
-        f.close()
+    def createTables(self, userName):
+        self.createDataTable(userName)
+        self.createHeaderTable(userName)
         return
 
-    def addLocation(locationString):
+    def addLocation(self, locationString):
         """
         Add the location of files to be reduced
         """
         self.location = locationString
-        addFiles(locationString)
+        self.addFiles(locationString)
         return
 
-    def addFiles(locationString):
+    def addFiles(self, locationString):
         """
         Filter the files so only text files are reduced
         """
-        allFiles = os.listdir(dataFolder)
+        allFiles = os.listdir(self.location)
         self.files = filter(lambda x: ".txt" in x, allFiles)
         return
 
     #Private Methods
 
-    def dataOrHeader(name, doH):
+    def dataOrHeader(self, name, doH):
         """
         Return either the second half (data, dOH==True) or
         first half of string (header, dOH==False)
         """
-        f = open(self.location + "\\" + name)
+        f = open(self.location + "/" + name)
         r = f.read()
         f.close()
         index = r.find(self.dividerString_)
         dataOrHeader = r[index+1:len(r)] if doH else r[0:index] 
         return dataOrHeader
     
-    def getData(name):
-        return dataOrHeader(name, True)
+    def getData(self, name):
+        return self.dataOrHeader(name, True)
 
-    def getHeader(name):
-        return dataOrHeader(name, False)
+    def getHeader(self, name):
+        return self.dataOrHeader(name, False)
 
-    def listOfListData(dataString):
+    def listOfListData(self, dataString):
         splitLines = dataString.split("\n")
         return map(lambda x: x.split(";")[:-1], splitLines)[:-1]
 
-    def lolHeader(headerString):
-        lineSplit = headerString.split("\n")
+    def lolHeader(self, headerString):
+        lineSplit = headerString.split("\r\n")
         return map(lambda x: x.split(";"), lineSplit)
 
-    def getVector(DataIndex, LOLData):
+    def getVector(self, DataIndex, LOLData):
         return map(lambda x: x[DataIndex], LOLData)
 
-    def getRaman(LOLData):
-        return getVector(RAMANSHIFT, LOLData)
+    def getRaman(self, LOLData):
+        return self.getVector(self.RAMANSHIFT, LOLData)
 
-    def getDark(LOLData):
-        return getVector(DARK, LOLData)
+    def getDark(self, LOLData):
+        return self.getVector(self.DARK, LOLData)
 
-    def getRaw(LOLData):
-        return getVector(RAWDATA, LOLData)
+    def getRaw(self, LOLData):
+        return self.getVector(self.RAWDATA, LOLData)
 
-    def processExp(ExpData):
+    def processExp(self, ExpData):
         """
         This method takes in the data string and performs some
         light processing. The dark signal is subtracted from the
         response and the raman shift is zipped to this resulting value
         """
-        LOLData = listOfListData(ExpData)
-        raman = getRaman(LOLData)[self.START:self.END]
-        dark = getDark(LOLData)[self.START:self.END]
-        raw = getRaw(LOLData)[self.START:self.END]
+        LOLData = self.listOfListData(ExpData)
+        raman = self.getRaman(LOLData)[self.START:self.END]
+        dark = self.getDark(LOLData)[self.START:self.END]
+        raw = self.getRaw(LOLData)[self.START:self.END]
         dataSeries = map(lambda x,y: str(float(x)-float(y)), raw,dark)
         #r = map(lambda x: x, raman)
         return map(lambda x: [x[0],x[1]], zip(raman, dataSeries))
 
-    def combineData(dataLOLA, dataLOLB):
+    def combineData(self, dataLOLA, dataLOLB):
         """
         Every file has a common raman shift we only 
         really need to include it from the first one (that's why
@@ -125,153 +116,44 @@ class TableCreator:
         return dataLOLA
 
 
-    def LoLToCSV(LoL):
+    def LoLToCSV(self, LoL):
         """
         Convert a list of lists into a list where each element is one
         long string and values are separated by a comma
         """
         return map(lambda x: reduce(lambda a,b: a+','+b, x),LoL)
 
-    def titleString():
+    def titleString(self):
         titles = reduce(lambda x,y: x + "," + y, self.files)
         return "Raman Shift," + titles + "\n"
 
 
-    def restString(LOLCSV):
-        rest = reduce(lambda x,y: x + "\n" + y, LOLCSV)
+    def restString(self, LoLCSV):
+        rest = reduce(lambda x,y: x + "\n" + y, LoLCSV)
         return rest
 
-    def createTable(userName, doH):
-        lList = map(lambda x: processExp(getData(x)), self.files) if dOH
-        else map(lambda x: lolHeader(getHeader(x)), self.files)
+    def createTable(self, userName, doH):
+        lList = map(lambda x: 
+        self.processExp(self.getData(x)) if doH else 
+        self.lolHeader(self.getHeader(x)) , self.files)
         # this method is kinda magic
-        zz = reduce(combineData, lList)
-        commaSeparatedElements = LOLToCSV(zz)
-        writeString = titleString() + restString(commaSeparatedElements)
+        zz = reduce(self.combineData, lList)
+        commaSepElements = self.LoLToCSV(zz)
+        writeString = self.titleString() + self.restString(commaSepElements)
         extension = "Data.txt" if doH else "Header.txt"
         f = open(userName + extension, 'w')
         f.write(writeString)
         f.close()
         return
 
-    def createDataTable(userName):
-        createTable(userName, True)
+    def createDataTable(self, userName):
+        self.createTable(userName, True)
         return
 
-    def createHeaderTable(userName):
-        createTable(userName, False)
+    def createHeaderTable(self, userName):
+        self.createTable(userName, False)
         return
 
-
-    
-
-
-
-            
-
-
-
-STARTFORDATA = "\nPixel"
-
-STARTFOR785 = 177 # 176 is start index on txt
-ENDFOR785 = 1980 # 1979 is the end
-indices785 = [STARTFOR785, ENDFOR785]
-
-STARTFOR1064 = 58 # 57 is start
-ENDFOR1064 = 486 # 485 is end
-indices1064 = [STARTFOR1064, ENDFOR1064]
-
-RAMANSHIFT = 3
-DARK = 4
-RAWDATA = 6
-dataFolder = "C:\Users\octopus\Documents\BwtekData"
-desktopLoc = "C:\Users\octopus\Desktop\Nov21 SNR Calcite"
-files = os.listdir(dataFolder)
-
-
-# gather files from a day
-def getFilesFromDay(dateString):
-    titles = filter(lambda x: dateString in x, files) 
-    return titles 
-
-# take in file path and retrieve string header
-# TODO MAKE THIS BETTER??
-def getData(name):
-    f = open(desktopLoc + "\\" + name)
-    r = f.read()
-    f.close()
-    startIndex = r.find(STARTFORDATA)
-    data = r[startIndex+1:len(r)]
-    return data
-
-def listOfListData(data):
-    splitLines = data.split("\n")
-    return map(lambda x: x.split(";")[:-1], splitLines)[:-1]
-
-def getVector(DataIndex, LOLData):
-    return map(lambda x: x[DataIndex], LOLData)
-
-def getRaman(LOLData):
-    return getVector(RAMANSHIFT, LOLData)
-
-def getDark(LOLData):
-    return getVector(DARK, LOLData)
-
-def getRaw(LOLData):
-    return getVector(RAWDATA, LOLData)
-
-#sampleDay = getFilesFromDay("Nov1")
-#sampleData = getData(sampleDay[0])
-
-def processExp(ExpData):
-    LOLData = listOfListData(ExpData)
-    [start, end] = indices1064
-    raman = getRaman(LOLData)[start:end]
-    dark = getDark(LOLData)[start:end]
-    raw = getRaw(LOLData)[start:end]
-    dataSeries = map(lambda x,y: str(float(x)-float(y)), raw,dark)
-    r = map(lambda x: x, raman)
-    return map(lambda x: [x[0],x[1]], zip(r, dataSeries))
-
-def combineData(dataLOLA, dataLOLB):
-    map(lambda x: x[0].append(x[1][1]), zip(dataLOLA, dataLOLB))
-    return dataLOLA
-
-# convert LOL to comma separation
-def LOLToCSV(lolHList):
-    return map(lambda x: reduce(lambda a,b: a+','+b, x),lolHList)
-
-def titleString(dateString):
-    #titles = getFilesFromDay(dateString)
-    titles = os.listdir(desktopLoc)
-    return "Raman Shift," + reduce(lambda x,y: x + "," + y, titles)+ "\n"
-
-# convert LOS to \n string
-def restString(LOLCSV):
-    rest = reduce(lambda x,y: x + "\n" + y, LOLCSV)
-    return rest
-
-def createDataTxt(dateString):
-    sampleDay = getFilesFromDay(dateString)
-    lDList = map(lambda x: processExp(getData(x)), sampleDay)
-    zz = reduce(combineData, lDList)
-    commaSeparatedElements = LOLToCSV(zz)
-    writeString = titleString(dateString) + restString(commaSeparatedElements) 
-    f = open(dateString + "Data.txt", 'w')
-    f.write(writeString)
-    f.close()
-    return
-
-def createDataFromFolder(dateString):
-    sampleDay = os.listdir(desktopLoc)
-    lDList = map(lambda x: processExp(getData(x)), sampleDay)
-    zz = reduce(combineData, lDList)
-    commaSeparatedElements = LOLToCSV(zz)
-    writeString = titleString(dateString) + restString(commaSeparatedElements) 
-    f = open(dateString + "Data.txt", 'w')
-    f.write(writeString)
-    f.close()
-    return
-
-
-
+d = TableCreator(785)
+d.addLocation("/Users/alexanderechevarria/Desktop/Microscope Nov 21")
+d.createTables("poop")
