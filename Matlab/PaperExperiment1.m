05e113210b969383935c6e3d526ceded819c3616
Alexander Echevarria <alexanderechevarria@WL-204-124.WPA.Claremont.Edu>
2017-05-03 23:48:39 -0700

    adding comparison

--- Matlab/PaperExperiment1.m
+++ Matlab/PaperExperiment1.m
@@ -18,8 +18,8 @@ ds_5 = getDataSet('4/7/2017');
 %% Apply transforms to normalize area and correct for fluorescence
 % corr_1 = ds_1.getCorr();
 % corr_5 = ds_5.getCorr();
-% norm_1 = ds_1.getNorm();
-% norm_5 = ds_5.getNorm();
+norm_1 = ds_1.getNorm();
+norm_5 = ds_5.getNorm();
 normCorr_1 = ds_1.getNormCorr();
 normCorr_5 = ds_5.getNormCorr();
 
@@ -40,6 +40,7 @@ havg_1 = mean(healthy_1,2);
 tumor_1 = normCorr_1(1:end, 30:end);
 tavg_1 = mean(tumor_1, 2);
 
+
 % not the case for 4/7 lol
 
 % numbers span 16 - 65 leave out 
@@ -50,13 +51,19 @@ havg_5 = mean(healthy_5,2);
 tumor_5 = normCorr_5(1:end, [23:26 40:46 49 51:54 60 62:63]); %tumor columns
 tavg_5 = mean(tumor_5,2);
 
+healthyN_5 = norm_5(1:end, [17:22 29:37 39 55:59 61 64:65]); %healthy colums
+havgN_5 = mean(healthyN_5,2);
+tumorN_5 = norm_5(1:end, [23:26 40:46 49 51:54 60 62:63]); %tumor columns
+tavgN_5 = mean(tumorN_5,2);
+
 subplot(2,1,1)
 hold on
 
-plot(wavenumbers_1, havg_1);
-plot(wavenumbers_1, tavg_1);
+plot(wavenumbers_5, havgN_5);
+plot(wavenumbers_5, tavgN_5);
 xlim([250 3200])
-title('Avg. Corr. & Normalized Spectra; ex=1064nm; n=27 healthy, 28 neoplasia; time=10sec, scans=3')
+title('Avg. Normalized Spectra; ex=785nm; n=24 healthy, 19 neoplasia; time=10sec, scans=1')
+%title('Avg. Normalized Spectra; ex=1064nm; n=27 healthy, 28 neoplasia; time=10sec, scans=3')
 ylabel('Rel. Intensity (arb. units)')
 xlabel('Wavenumber cm^-^1')
 
@@ -66,7 +73,7 @@ hold on
 plot(wavenumbers_5, havg_5);
 plot(wavenumbers_5, tavg_5);
 xlim([250 3200])
-title('Avg. Corr. & Normalized Spectra; ex=785nm; n=24 healthy, 19 neoplasia; time=10sec, scans=1')
+title('Avg. Normalized & Corrected Spectra; ex=785nm; n=24 healthy, 19 neoplasia; time=10sec, scans=1')
 ylabel('Rel. Intensity (arb. units)')
 xlabel('Wavenumber cm^-^1')
 
